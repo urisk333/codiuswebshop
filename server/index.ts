e5eee425f0453d56bbd2fb32082367a89228db39
urisk <carst333@gmail.com>
2022-02-16 21:33:55 +0100

    [APP: DEPLOYMENT]: Update the docs for deployment

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,23 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import router from './router';
+import path from 'path';
 
 const app: Application = express();
 const SERVER_PORT = 3001;
 
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 (async function (): Promise<void> {
   try {
-    app.listen(SERVER_PORT, (): void => {
+    app.listen(process.env.PORT || SERVER_PORT, (): void => {
       console.log(`Server is UP at http://localhost:${SERVER_PORT}`);
     });
   } catch (err) {
